162a608e937f23611dfeaf2aa0d9c6c093afa60f
tiffanyadu <tadofdesign@gmail.com>
2019-02-28 22:37:24 -0600

    Create and wire fetchPosts action

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,6 +1,11 @@
 import React from "react";
+import { connect } from "react-redux";
+import { fetchPosts } from "../actions";
 
 class PostList extends React.Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
   render() {
     return (
       <div>
@@ -10,4 +15,9 @@ class PostList extends React.Component {
   }
 }
 
-export default PostList;
+export default connect(
+  null,
+  {
+    fetchPosts
+  }
+)(PostList);
