d5d13fc1fae722257aa652359b8bd2db347782fe
tiffanyadu <tadofdesign@gmail.com>
2019-02-28 22:14:50 -0600

    Initialize PostList component

---
+++ src/components/PostList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class PostList extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>PostList</h1>
+      </div>
+    );
+  }
+}
+
+export default PostList;
