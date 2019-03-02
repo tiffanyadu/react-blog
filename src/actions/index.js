d82d304eafd70770c0eff86dd96215fe8124fbd7
tiffanyadu <tadofdesign@gmail.com>
2019-03-01 22:43:15 -0600

    Fix error

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,11 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPosts = async () => {
-  const response = await jsonPlaceholder.get("posts");
-  return {
-    type: "FETCH_POSTS",
-    payload: response
+export const fetchPosts = () => {
+  return function(dispatch, getState) {
+    const promise = jsonPlaceholder.get("posts");
+    return {
+      type: "FETCH_POSTS",
+      payload: promise
+    };
   };
 };
