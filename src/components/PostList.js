fc932f5dba695d9dd46fdb0427e63d23cc41e3b5
tiffanyadu <tadofdesign@gmail.com>
2019-03-03 18:26:35 -0600

    Add mapStateToProps and pass returned data as component props

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { connect } from "react-redux";
 import { fetchPosts } from "../actions";
+// import postsReducer from "../reduers";
 
 class PostList extends React.Component {
   componentDidMount() {
     this.props.fetchPosts();
   }
   render() {
+    console.log(this.props.posts);
     return (
       <div>
         <h1>PostList</h1>
@@ -14,9 +16,11 @@ class PostList extends React.Component {
     );
   }
 }
-
+const mapStateToProps = state => {
+  return { posts: state.posts };
+};
 export default connect(
-  null,
+  mapStateToProps,
   {
     fetchPosts
   }
