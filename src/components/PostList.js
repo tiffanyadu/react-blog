4fc92421e09fd06ce0ce623984e99d5906c10c4d
tiffanyadu <tadofdesign@gmail.com>
2019-03-05 21:25:10 -0600

    Create renderList function to map through list of posts and render to page

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -7,13 +7,24 @@ class PostList extends React.Component {
   componentDidMount() {
     this.props.fetchPosts();
   }
+  renderList() {
+    return this.props.posts.map(post => {
+      return (
+        <div className="item" key={post.id}>
+          <i className="large middle aligned icon user" />
+          <div className="content">
+            <div className="description">
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  }
   render() {
     console.log(this.props.posts);
-    return (
-      <div>
-        <h1>PostList</h1>
-      </div>
-    );
+    return <div className="ui relaxed divided list">{this.renderList()}</div>;
   }
 }
 const mapStateToProps = state => {
