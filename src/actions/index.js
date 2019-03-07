2dc52bea475b460b507c8b3b6c00c1610e280066
tiffanyadu <tadofdesign@gmail.com>
2019-03-06 21:57:10 -0600

    Create fetchPostsAndUsers action to avoid overfetching

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,12 @@
 import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
+export const fetchPostsAndUsers = () => async dispatch => {
+  console.log("About to fetch posts");
+  await dispatch(fetchPosts());
+  console.log("fetched posts!");
+};
+
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get("posts");
   dispatch({ type: "FETCH_POSTS", payload: response.data });
