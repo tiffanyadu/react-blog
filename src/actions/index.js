a0754b124b187df7d915e6b8779c3ed2c06eea97
tiffanyadu <tadofdesign@gmail.com>
2019-03-06 21:11:26 -0600

    Refactor memoized function further to simplify

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,9 +6,7 @@ export const fetchPosts = () => async dispatch => {
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-export const fetchUser = id => dispatch => {
-  _fetchUser(id, dispatch);
-};
+export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
 const _fetchUser = _.memoize(async (id, dispatch) => {
   const response = await jsonPlaceholder.get(`users/${id}`);
   dispatch({ type: "FETCH_USER", payload: response.data });
